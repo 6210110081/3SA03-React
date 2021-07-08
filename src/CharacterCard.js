ecdeeef064399b2c413b047bcffb7104d4fd226b
6210110081 <6210110081@psu.ac.th>
2021-07-08 10:22:54 +0700

    add file CharecterCard

---
+++ src/CharacterCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+export default function CharacterCard(props) {
+    return (
+        <div>{props.value}</div>
+    )
+}
+แก้ไขไฟล์ App.js
+import CharacterCard from './CharacterCard';
+function App() {
+    return (
+        <div>
+            <CharacterCard value="h" />
+            <CharacterCard value="i" />
+        </div>
+    );
+}
