d5245c6bed4efb78d290939e1056072548880d8b
6210110081 <6210110081@psu.ac.th>
2021-07-08 10:41:47 +0700

    4. Components & Props

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,8 +1,12 @@
 import Reac from 'react'
+import CharacterCard from './CharacterCard'
+
 export default function WordCard(props) {
-    return {
-    < div >
-    
-        <div />
-}
+    return (
+        <div>
+            {
+                Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} />)
+            }
+        </div>
+    )
 }
