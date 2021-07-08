6b6bc2e71a4240e70cce425c3c23aed1041e9663
6210110081 <6210110081@psu.ac.th>
2021-07-08 11:17:55 +0700

    check guess in console

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,15 +1,35 @@
-import Reac from 'react'
+import React, { useState } from 'react'
+import _ from 'lodash'
+
 import CharacterCard from './CharacterCard'
+
+
+
+const prepareStateFromWorld = given_word => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        complete: false
+    }
+}
 
 export default function WordCard(props) {
 
+    const [state, setState] = useState(prepareStateFromWorld(props.value))
+
     const activationHandler = c => {
         console.log(`${c} has been activated.`)
+
+        let
     }
     return (
         <div>
             {
-                Array.from(props.value).map((c, i) =>
+                state.chars.map((c, i) =>
                     <CharacterCard value={c} key={i} activationHandler={activationHandler} />
                 )
             }
