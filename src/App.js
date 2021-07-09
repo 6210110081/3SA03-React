5052e9acdf7877ee57eeb301edfb2cbd88ad31b9
6210110081 <6210110081@psu.ac.th>
2021-07-09 12:11:44 +0700

    Hint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './App.css';
+import HintCard from './HintCard';
 import WordCard from './WordCard';
 
-const word ="Hello";
+const word = "Hello";
+const hint ='Hint'
 function App() {
-    return (
-        <div>
-          {
-            <WordCard value={word}/>
-          }
-        </div>
-    );
+  return (
+    <div>
+      <WordCard value={word} />
+      <HintCard value={hint} />
+    </div>
+
+  );
 }
 export default App;
